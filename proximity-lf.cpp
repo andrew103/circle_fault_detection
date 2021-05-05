ef7fb59dfe813d147b3cc22b75f8deef54750dcc
Revant Mahajan <revantmahajan1999@gmail.com>
2021-05-04 21:44:31 -0400

    Circle behavior implemented

---
+++ proximity-lf.cpp
@@ -0,0 +1,6 @@
+#include "proximity-lf.h"
+
+/****************************************/
+/****************************************/
+
+REGISTER_LOOP_FUNCTIONS(CProximityLF, "proximity-lf");
---
+++ proximity-lf.h
@@ -0,0 +1,15 @@
+#ifndef PROXIMITY_H
+#define PROXIMITY_H
+
+#include <buzz/argos/buzz_loop_functions.h>
+
+class CProximityLF : public CBuzzLoopFunctions {
+
+public:
+
+   CProximityLF() {}
+   ~CProximityLF() override {}
+
+};
+
+#endif
